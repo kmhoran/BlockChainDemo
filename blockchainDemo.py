222a1d45a5110f539b94509252daca203c1722b3
kmhoran <kevin.michael.horan@gmail.com>
2018-02-22 20:25:24 -0800

    add some comments to the demo

--- blockchainDemo.py
+++ blockchainDemo.py
@@ -1,23 +1,27 @@
-# import genesis_block as start
-# from block import Block
 from chain import Chain
 
 def run():
+    # Create chain to hold ledger
     chain = Chain()
+
     print("\n## Created chain with origin block. ##\n")
     print(chain)
     print("\n")
+
+    # Add blocks
     chain.add_block("This is the first real block!")
     print("    >> Added block 1. <<\n")
+
     chain.add_block({'name': 'new block', 'type':'test'})
     print("    >> Added block 2. <<\n")
+
     chain.add_block({'name': 'some other type of data', 'series':['a', 'b', 'c']})
     print("    >> Added block 3. <<\n")
+
+    # Let's take a look at our chain
     for i in range(len(chain.ledger)):
         print(chain.ledger[i])
         print("\n")
-
-
 
 if __name__ == "__main__":
     run()
